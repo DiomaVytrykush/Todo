8d679a4990839a39a54f8d8254fde2ea532fe0ee
Dioma <dioma@gmail.com>
2020-08-13 13:18:28 +0300

    Updates

---
+++ precache-manifest.e5b3fa303d94fb9927b363ff796cf884.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33f2605d3a01ccfb1717ec618dc387c0",
+    "url": "/Todo/index.html"
+  },
+  {
+    "revision": "228720c79133185b92ea",
+    "url": "/Todo/static/css/3.b277acaa.chunk.css"
+  },
+  {
+    "revision": "e66940bc513b56c6acc3",
+    "url": "/Todo/static/css/main.19925b93.chunk.css"
+  },
+  {
+    "revision": "94c6243cad2479655d34",
+    "url": "/Todo/static/js/2.44bfc445.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo/static/js/2.44bfc445.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "228720c79133185b92ea",
+    "url": "/Todo/static/js/3.91e7444c.chunk.js"
+  },
+  {
+    "revision": "e66940bc513b56c6acc3",
+    "url": "/Todo/static/js/main.43c08c67.chunk.js"
+  },
+  {
+    "revision": "8b2d3b335bf0f2a924cb",
+    "url": "/Todo/static/js/runtime-main.25b4ebaa.js"
+  }
+]);
